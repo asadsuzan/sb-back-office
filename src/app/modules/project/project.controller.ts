15d9bbfca6fc20bead94b718335775e365d5e40f
suzan <asadsuzan7@gmail.com>
2025-05-25 12:53:02 +0600

    feat(project): add project get,update and delete by slug feature

--- src/app/modules/project/project.controller.ts
+++ src/app/modules/project/project.controller.ts
@@ -8,7 +8,6 @@ import sendSuccessResponse from "../../utils/successResponse";
 // @desc    Create a new project
 export const createProject = asyncHandler(async (req: Request, res: Response) => {
   const project = await projectService.createProject(req.body);
-
   sendSuccessResponse(res, 201, "Project created successfully", project);
 });
 
@@ -20,3 +19,59 @@ export const getAllProjects = asyncHandler(async (req: Request, res: Response) =
 
 }
 );
+
+// @desc    Get a project by slug
+export const getProjectBySlug = asyncHandler(async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  if (!slug) {
+   sendSuccessResponse(res, 400, "Slug is required");
+    return;
+  }
+  const project = await projectService.getProjectBySlug(slug);
+
+  if (!project) {
+  
+    sendSuccessResponse(res, 404, "Project not found");
+    return;
+  }
+
+  sendSuccessResponse(res, 200, "Project fetched successfully", project);
+});
+
+// @desc    Update a project by slug
+export const updateProjectBySlug = asyncHandler(async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  if (!slug) {
+   sendSuccessResponse(res, 400, "Slug is required");
+
+    return;
+  }
+  const updatedProject = await projectService.updateProjectBySlug(slug, req.body);
+
+  if (!updatedProject) {
+  
+    sendSuccessResponse(res, 404, "Project not found");
+    return;
+  }
+
+  sendSuccessResponse(res, 200, "Project updated successfully", updatedProject);
+});
+
+// @desc    Delete a project by slug
+export const deleteProjectBySlug = asyncHandler(async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  if (!slug) {
+    
+    sendSuccessResponse(res, 400, "Slug is required");
+    return;
+  }
+  const deletedProject = await projectService.deleteProjectBySlug(slug);
+
+  if (!deletedProject) {
+   
+    sendSuccessResponse(res, 404, "Project not found");
+    return;
+  }
+
+  sendSuccessResponse(res, 200, "Project deleted successfully", deletedProject);
+});
--- src/app/modules/project/project.route.ts
+++ src/app/modules/project/project.route.ts
@@ -6,6 +6,9 @@ const router = Router();
 
 router.post("/", projectController.createProject);
 router.get("/", projectController.getAllProjects);
+router.get("/:slug", projectController.getProjectBySlug);
+router.put("/:slug", projectController.updateProjectBySlug);
+router.delete("/:slug", projectController.deleteProjectBySlug);
 
 
 export default router;
