fdff3caeaad7468cbcdaef4b1d391bd3d57300fb
suzan <asadsuzan7@gmail.com>
2025-06-01 23:34:10 +0600

    refactor: Standardize project queries to use basicInfo.slug
    This change updates the project service functions (getProjectBySlug, updateProjectBySlug, and deleteProjectBySlug) to consistently use the basicInfo.slug field for database lookups. This ensures consistency with the project data structure and improves data retrieval reliability.

--- src/app/modules/project/project.service.ts
+++ src/app/modules/project/project.service.ts
@@ -12,16 +12,16 @@ export const getAllProjects = async () => {
 };
 
 export const getProjectBySlug = async (slug: string) => {
-  return await Project.findOne({ slug });
+  return await Project.findOne({ "basicInfo.slug":slug });
 };
 
 export const updateProjectBySlug = async (
   slug: string,
   data: Partial<IProject>,
 ) => {
-  return await Project.findOneAndUpdate({ slug }, data, { new: true });
+  return await Project.findOneAndUpdate({ "basicInfo.slug":slug }, data, { new: true });
 };
 
 export const deleteProjectBySlug = async (slug: string) => {
-  return await Project.findOneAndDelete({ slug });
+  return await Project.findOneAndDelete({ "basicInfo.slug":slug });
 };
